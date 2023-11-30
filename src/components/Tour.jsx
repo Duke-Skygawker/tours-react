40d08d96ad2e5e029e35bed9f86ba7dd211683b0
Duke-Skygawker <bart3kk990@gmail.com>
2023-11-30 06:49:20 +0100

    Tour dummy data for styles

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,17 @@
 import logo from "../../public/vite.svg";
+const lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eius esse, aliquam, repellat unde ad debitis cumque architecto eum delectus, odio corporis sequi omnis numquam!";
 
 const Tour = ({ name, info, image, price, removeThis }) => {
   return (
     <article>
       <div>
-        <img src={image || logo} alt={name} />
-        <p>{price}</p>
+        <img src={image || logo} alt={name || "tour photo"} />
+        <p>{price || "$9,999"}</p>
       </div>
       <div>
-        <h4>{name}</h4>
-        <p>{info}</p>
+        <h4>{name || "Journey to the center of the Earth"}</h4>
+        <p>{info || lorem}</p>
         <button>Read More</button>
       </div>
       <button className="btn" type="button" onClick={removeThis}>
