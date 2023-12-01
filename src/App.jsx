99e21943bbdcf0b4046c00d3e8ae7530e2b5fd47
Duke-Skygawker <bart3kk990@gmail.com>
2023-12-01 03:11:40 +0100

    reload tours functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,28 +6,28 @@ const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
   const [data, setData] = useState(null);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+      setData(tours);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+    setIsLoading(false);
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const tours = await response.json();
-        console.log(tours);
-        setData(tours);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-        setIsError(true);
-      }
-      setIsLoading(false);
-    };
     fetchData();
   }, []);
 
   const reloadTours = () => {
-    console.log("tryna reload offers innit");
+    fetchData();
   };
 
   return (
@@ -47,7 +47,11 @@ const App = () => {
         ) : (
           <div>
             <Tours array={data} />
-            <button className="btn" type="button" onClick={reloadTours}>
+            <button
+              className="btn reload-btn"
+              type="button"
+              onClick={reloadTours}
+            >
               Reload Tour Offers
             </button>
           </div>
